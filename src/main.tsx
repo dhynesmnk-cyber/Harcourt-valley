36df99faf924da711fa34871838f5de3e11f6571
David <d.hynes.mnk@gmail.com>
2026-08-18 09:16:57 +1000

    Fix: add error boundary to prevent white screens

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import ErrorBoundary from "./components/ErrorBoundary";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+  <React.StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
+);
