ed413f0867247962e67d6a8601ef83eebaf64f62
David <d.hynes.mnk@gmail.com>
2026-08-18 11:50:43 +1000

    Fix main.tsx syntax error

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import App from "./App";
 import ErrorBoundary from "./components/ErrorBoundary";
 
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ErrorBoundary>
       <App />
